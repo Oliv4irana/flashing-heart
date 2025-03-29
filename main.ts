99710d16acffcbe494f0e9a3d54a9c118191e04f
Oliv4irana <anasilva09052008@gmail.com>
2025-03-29 17:46:47 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,66 @@
+basic.forever(function () {
+    basic.showString("Ana Victoria 3C")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
